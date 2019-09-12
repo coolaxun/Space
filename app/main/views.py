b913baff9049316413f28684e0d1ac05610b8930
arnxun <flyqhzx@qq.com>
2019-09-12 08:10:10 +0800

    create dev branch

--- app/main/views.py
+++ app/main/views.py
@@ -3,13 +3,13 @@ from flask import render_template, session, redirect, url_for
 
 from . import main
 from .forms import NameForm
-from ..models import Post
+from app.models import User
 from .. import db
 
 
 @main.route('/')
 @main.route('/index')
 def index():
-    posts = Post.query.all()
+    posts = User.query.all()
     return render_template('main/index.html', title='首页', name=session.get('name'), posts=posts)
 
--- app/models.py
+++ app/models.py
@@ -1,23 +1,26 @@
 from werkzeug.security import generate_password_hash, check_password_hash
-from app import db
 from flask_login import UserMixin
 from app import login_manager
 from datetime import datetime
 from flask import current_app
 
+from app import db
+
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer, SignatureExpired, BadSignature
 
 
 class User(UserMixin, db.Model):
-    __tablename__ = 'users'
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
     email = db.Column(db.String(64), unique=True, index=True)
-    roles = db.relationship('Role', backref='user', lazy='dynamic')
-    # role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    phone = db.Column(db.String(64), unique=True)
+    create_time = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    # roles = db.relationship('Role', secondary=user_roles, backref='users', lazy='dynamic')
 
-    # posts = db.relationship('POST', backref='user', lazy='dynamic')
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
+    role = db.relationship('Role', backref=db.backref('users', lazy='dynamic'), lazy='dynamic')
 
     @property
     def password(self):
@@ -47,41 +50,85 @@ class User(UserMixin, db.Model):
         return user
 
 
+# relationship table
+# the relation between Role and Permission is many-to-many
+permission_role = db.Table('permission_role',
+                     db.Column('role_id', db.Integer, db.ForeignKey('role.id')),
+                     db.Column('permission_id', db.Integer, db.ForeignKey('permission.id'))
+                     )
+
+
+# relationship table
+# the relation between Permission and Resource is many-to-many
+permission_resource = db.Table('permission_resource',
+                     db.Column('permission_id', db.Integer, db.ForeignKey('permission.id')),
+                     db.Column('resource_id', db.Integer, db.ForeignKey('resource.id'))
+                     )
+
+
 class Role(db.Model):
-    __tablename__ = 'roles'
+    __tablename__ = 'role'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
     default = db.Column(db.Boolean, default=False, index=True)
+    enabled = db.Column(db.Boolean, default=True)
     permissions = db.Column(db.Integer)
-    # users = db.relationship('User', backref='role', lazy='dynamic')
+    create_time = db.Column(db.DateTime, default=datetime.utcnow)
+    # users = db.relationship('User', backref='roles', lazy='dynamic')
+    perms = db.relationship('Permission', secondary=permission_role, backref=db.backref('roles', lazy='dynamic'))
 
 
-# class UserProfile(db.Model):
-#     __tablename__ = 'user_profiles'
-#
-
-
-class Post(db.Model):
-    __tablename__ = 'posts'
+class Permission(db.Model):
+    __tablename__ = 'permission'
     id = db.Column(db.Integer, primary_key=True)
-    # title = db.Column(db.String(128), nullable=False)
-    title = db.Column(db.String(128))
-    body = db.Column(db.Text)
-    create_time = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    code = db.Column(db.String(10), unique=True)
+    name = db.Column(db.String(64))
+    enabled = db.Column(db.Boolean, default=True)
+    create_time = db.Column(db.DateTime, default=datetime.utcnow)
+    resources = db.relationship('Resource', secondary=permission_resource, backref=db.backref('perms', lazy='dynamic'))
 
-    def to_json(self):
-        json_post = {
-            'body': self.body
-        }
-        return json_post
+
+class Resource(db.Model):
+    __tablename__ = 'resource'
+    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.String(10), unique=True)
+    name = db.Column(db.String(64))
+    url = db.Column(db.String(64))
+    enabled = db.Column(db.Boolean, default=True)
+    func = db.Column(db.String(64))
+    create_time = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+class Menu(db.Model):
+    __tablename__ = 'menu'
+    id = db.Column(db.Integer, primary_key=True)
+    alias = db.Column(db.String(10), unique=True)
+    name = db.Column(db.String(64))
+    parent_id = db.Column(db.Integer)
+    visible = db.Column(db.Boolean, default=True)
+    enabled = db.Column(db.Boolean, default=True)
+    create_time = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+# class Post(db.Model):
+#     __tablename__ = 'posts'
+#     id = db.Column(db.Integer, primary_key=True)
+#     # title = db.Column(db.String(128), nullable=False)
+#     title = db.Column(db.String(128))
+#     body = db.Column(db.Text)
+#     create_time = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+#     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+#
+#     def to_json(self):
+#         json_post = {
+#             'body': self.body
+#         }
+#         return json_post
 
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-
-class Permission:
-    FOLLOW = 0x01
-
+# class Permission:
+#     FOLLOW = 0x01
